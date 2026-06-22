4bf36afa2a29eef08118666a8bbf6719e339ab98
Hector RE <heatra@hotmail.comgit config --list>
2026-06-21 23:05:38 -0300

    commit7

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,10 @@
 
-const crearMenu = () => {
+console.log("menu.js cargado");
 
+
+
+const crearMenu = () => {
+    console.log("crearMenu ejecutado");
     const menu = document.getElementById("menu");
 
     menu.innerHTML = `
@@ -9,7 +13,7 @@ const crearMenu = () => {
                 <li><a href="../index.html"><strong>Inicio</strong></a></li>
                 <li><a href="../pages/productos.html"><strong>Productos</strong></a></li>
                 <li> <a href="../pages/servicios.html"><strong>Servicios</strong></a></li>
-                <li><a href="../pages/reseñas.html"><strong>Reseñas</strong></a></li>
+                <li><a href="../pages/resenas.html"><strong>Reseñas</strong></a></li>
                 <li><a href="../pages/contacto.html"><strong>Contacto</strong></a></li>
             </ul>
 
