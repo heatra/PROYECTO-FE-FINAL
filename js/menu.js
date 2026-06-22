cb84f3d710af6e5b04a159c3c55930f5597e0afe
Hector RE <heatra@hotmail.comgit config --list>
2026-06-21 23:43:12 -0300

    commit9

--- js/menu.js
+++ js/menu.js
@@ -7,14 +7,22 @@ const crearMenu = () => {
     console.log("crearMenu ejecutado");
     const menu = document.getElementById("menu");
 
+
+
+    // Detecta automáticamente la carpeta "pages"
+    const esPaginaSecundaria = window.location.pathname.includes("/pages/");
+    const prefijo = esPaginaSecundaria ? "../" : "./";
+
+
+
     menu.innerHTML = `
         <nav class="menu">  
              <ul>
-                <li><a href="../index.html"><strong>Inicio</strong></a></li>
-                <li><a href="../pages/productos.html"><strong>Productos</strong></a></li>
-                <li> <a href="../pages/servicios.html"><strong>Servicios</strong></a></li>
-                <li><a href="../pages/resenas.html"><strong>Reseñas</strong></a></li>
-                <li><a href="../pages/contacto.html"><strong>Contacto</strong></a></li>
+                <li><a href="${prefijo}index.html"><strong>Inicio</strong></a></li>
+                <li><a href="${prefijo}pages/productos.html"><strong>Productos</strong></a></li>
+                <li> <a href="${prefijo}pages/servicios.html"><strong>Servicios</strong></a></li>
+                <li><a href="${prefijo}pages/resenas.html"><strong>Reseñas</strong></a></li>
+                <li><a href="${prefijo}pages/contacto.html"><strong>Contacto</strong></a></li>
             </ul>
 
         </nav>
